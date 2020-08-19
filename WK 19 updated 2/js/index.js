a4988b8e2399141c0b27c40bb91e369b86d9ba8f
jduncan5 <61210240+jduncan5@users.noreply.github.com>
2020-08-19 01:03:14 -0400

    HW

---
+++ WK 19 updated 2/js/index.js
@@ -0,0 +1,26 @@
+
+setTimeout(function() {
+  $('.preloader').fadeOut("slow");
+}, 3000);
+
+$(".toggle").click(function(){
+  $(this).find(".toggleContainer").toggleClass("active");
+  $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+});
+
+$(".searchButton").hover(function(){
+  $(".searchButton").toggleClass("active");
+  if ($(".searchButton").hasClass("active")) {
+    $(".searchBar").css("height", "100vh");
+    $("#searchForm").css("opacity","1");
+    $(".searchIcon").removeClass("fa-search").addClass("fa-times");
+  }
+  else {
+    $(".searchBar").css('height', "0vh");
+    $("#searchForm").css("opacity","0");
+    $(".searchIcon").removeClass("fa-times").addClass("fa-search");
+  }
+});
+
+
+ 
